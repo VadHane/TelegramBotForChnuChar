569cddd41d7e8c4072f8ddc6fde661377698d1c2
Vadim Hanevych <vadhanevych@gmail.com>
2021-07-24 12:05:20 +0300

    Added new keyboards for admins command and verification some actions

--- Keyboards.cs
+++ Keyboards.cs
@@ -1,3 +1,4 @@
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace ChatBot
@@ -9,6 +10,41 @@ namespace ChatBot
             return new InlineKeyboardMarkup(new[]
             {
                 InlineKeyboardButton.WithUrl("Приєднатись", Settings.InviteLinkIntoMainChat),
+            });
+        }
+
+        public static InlineKeyboardMarkup DeletedMessage(Message message)
+        {
+            return new InlineKeyboardMarkup(new[]
+            {
+                new []
+                {
+                    InlineKeyboardButton.WithCallbackData("Warn", 
+                        $"Admin:Warn:{message.From.Username}:{message.From.FirstName} {message.From.LastName}"), 
+                },
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("Mute", $"Admin:Mute:{message.From.Id}"),
+                },
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("Kick", $"Admin:Kick:{message.From.Id}"),
+                }
+            });
+        }
+
+        public static InlineKeyboardMarkup VerificationAction(int actionId)
+        {
+            return new InlineKeyboardMarkup(new[]
+            {
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("Підтвердити", $"Action:Do:{actionId}"),
+                },
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("Відмінити", $"Action:Cansel:{actionId}"),
+                }
             });
         }
     }
