e9dde28a4f729524289bd19b2a6a79c39928919f
Vadim Hanevych <vadhanevych@gmail.com>
2021-07-24 12:07:40 +0300

    Added new settings

--- Settings.cs
+++ Settings.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace ChatBot
 {
     public static class Settings
@@ -6,5 +8,6 @@ namespace ChatBot
         public const long MainChat = 0;
         public const long LogChat = 0;
         public const string InviteLinkIntoMainChat = "";
+        public static List<int> Admins = new List<int>();
     }
 }
