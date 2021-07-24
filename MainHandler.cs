2a263affb6a025482d5e31632ca05dcdee2a25fc
Vadim Hanevych <vadhanevych@gmail.com>
2021-07-24 12:11:10 +0300

    Added basic logic of bot

--- MainHandler.cs
+++ MainHandler.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using ChatBot.Handlers;
 using ChatBot.Models;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types;
@@ -12,11 +13,56 @@ namespace ChatBot
         {
             if (e.Message.Chat.Type == ChatType.Group || e.Message.Chat.Type == ChatType.Supergroup)
             {
-                // some work
+                if (Settings.Admins.Exists(adm => adm == e.Message.From.Id))
+                {
+                    string[] cmd = e.Message.Text.Split(" ");
+
+                    switch (cmd[0])
+                    {
+                        case "/mute":
+                            
+                            break;
+                        case "/kick":
+                            
+                            break;
+                        default:
+                            return;
+                    }
+                }
+                else if (e.Message.Chat.Id == Settings.MainChat)
+                {
+                    if (e.Message.Text == "/rules")
+                    {
+                        // send rules into chat
+                    }
+                    else
+                    {
+                        Parse.CheckMessage(e.Message);
+                    }
+                }
+                else if ( e.Message.Chat.Id == Settings.LogChat)
+                {
+                    switch (e.Message.Text)
+                    {
+                        case "/cmd":
+                            // add word, del word, edit rules
+                            break;
+                        case "/help":
+                            
+                            break;
+                        default:
+                            await Program.bot.SafeDeleteMessageAsync(e.Message);
+                            break;
+                    }
+                }
+                else
+                {
+                    await Program.bot.LeaveChatAsync(e.Message.Chat);
+                }
             }
             else if (e.Message.Chat.Type == ChatType.Private)
             {
-                Program.bot.SafeSendMessageAsync(e.Message.Chat, Text.ForPrivedChat, ParseMode.Html,
+                await Program.bot.SafeSendMessageAsync(e.Message.Chat, Text.ForPrivedChat, ParseMode.Html,
                     Keyboards.InviteToChatKeyboard());
             }
             else
