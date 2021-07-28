1a2700ba2338b84c963e574d55bbdf8dff058d10
Vadim Hanevych <vadhanevych@gmail.com>
2021-07-28 09:12:43 +0300

    Added new safe function for editing user permission.

--- Models/SafeActions.cs
+++ Models/SafeActions.cs
@@ -25,13 +25,13 @@ namespace ChatBot.Models
             catch
             {
                 Console.WriteLine($"[Error] I can`t delete message! Check log-chat!");
-                Program.bot.SafeSendMessageAsync(Settings.LogChat, Text.CantDeleteMessage(message), ParseMode.Html);
+                await Program.bot.SafeSendMessageAsync(Settings.LogChat, Text.CantDeleteMessage(message), ParseMode.Html);
             }
         }
         
         
         /// <summary>
-        /// Function for safe sinding message into group or prived chat.
+        /// Function for safe sending message into group or prived chat.
         /// </summary>
         /// <param name="bot">Link to bot client.</param>
         /// <param name="chatId">Id of chat where you would like to send message.</param>
@@ -50,7 +50,43 @@ namespace ChatBot.Models
                 Console.WriteLine($"[Error] I can`t send message! (chatId - {chatId}, message - {text});");
             }
         }
-        
-        
+
+        /// <summary>
+        /// Function for safe deleting members from main chat.
+        /// </summary>
+        /// <param name="bot">Link to bot client.</param>
+        /// <param name="id">Unique id of member for deleting.</param>
+        public static async Task SafeDeleteMembersAsync(this TelegramBotClient bot, int id)
+        {
+            try
+            {
+                await Program.bot.KickChatMemberAsync(Settings.MainChat, id);
+            }
+            catch
+            {
+                Console.WriteLine($"[Error] I cant kick chat member!");
+                await Program.bot.SafeSendMessageAsync(Settings.LogChat, "Я не зміг кікнути цю людину!");
+            }
+        }
+
+        /// <summary>
+        /// Function for safe editing user`s permissions.
+        /// </summary>
+        /// <param name="bot">Link to bot client.</param>
+        /// <param name="userId">Telegram unique id for this student.</param>
+        /// <param name="canSendMessage">Permission for sending message.</param>
+        public static async Task<bool> SafeEditUserPermissionsAsync(this TelegramBotClient bot, int userId, bool canSendMessage)
+        {
+            try
+            {
+                await Program.bot.PromoteChatMemberAsync(Settings.MainChat, userId, canPostMessages: canSendMessage);
+                return true;
+            }
+            catch 
+            {
+                Console.WriteLine($"[Error] I cant edit permissaons chat member!");
+                return false;
+            }
+        }
     }
 }
