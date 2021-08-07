f032b0524c3cdcc6f10593c94dc10d1d54e9bb83
Vadim Hanevych <vadhanevych@gmail.com>
2021-08-07 18:18:03 +0300

    Created full logic. Added handlers for new commands. Added command /help and /cmd.

--- MainHandler.cs
+++ MainHandler.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using ChatBot.Handlers;
 using ChatBot.Models;
@@ -13,27 +14,91 @@ namespace ChatBot
         {
             if (e.Message.Chat.Type == ChatType.Group || e.Message.Chat.Type == ChatType.Supergroup)
             {
-                if (Settings.Admins.Exists(adm => adm == e.Message.From.Id))
+                if (e.Message.Text == null && e.Message.Type != MessageType.ChatMembersAdded)
                 {
-                    string[] cmd = e.Message.Text.Split(" ");
-
-                    switch (cmd[0])
-                    {
-                        case "/mute":
-                            
-                            break;
-                        case "/kick":
-                            
-                            break;
-                        default:
-                            return;
-                    }
+                    return;
                 }
-                else if (e.Message.Chat.Id == Settings.MainChat)
+
+                if (e.Message.Chat.Id == Settings.MainChat)
                 {
-                    if (e.Message.Text == "/rules")
+                    if (e.Message.Type == MessageType.ChatMembersAdded)
                     {
-                        // send rules into chat
+                        if (e.Message.NewChatMembers.FirstOrDefault(user => user.Id == Program.bot.BotId) == null)
+                        {
+                            await Rules.SendRules();
+                        }
+                    }
+                    else if (e.Message.Text == "/rules")
+                    {
+                        await Rules.SendRules();
+                    }
+                    else if (Settings.Admins.Exists(adm => adm == e.Message.From.Id))
+                    {
+                        string[] cmd = e.Message.Text.Split(" ");
+                        int time;
+
+                        if (e.Message.ReplyToMessage != null)
+                        {
+
+                            switch (cmd[0])
+                            {
+                                case "/mute":
+                                    await Program.bot.SafeDeleteMessageAsync(e.Message);
+                                    if (cmd.Length > 1 && int.TryParse(cmd[1], out time))
+                                    {
+                                        Action.AddAction(new Mute(
+                                            e.Message.ReplyToMessage.From.FirstName + " " +
+                                            e.Message.ReplyToMessage.From.LastName,
+                                            e.Message.ReplyToMessage.From.Id, time));
+                                    }
+                                    else
+                                    {
+                                        await Program.bot.SafeSendMessageAsync(Settings.LogChat,
+                                            "Я не зміг обмежити користувача: невірно введено час!");
+                                    }
+
+                                    break;
+                                case "/kick":
+                                    await Program.bot.SafeDeleteMessageAsync(e.Message);
+                                    Action.AddAction(new Kick(
+                                        e.Message.ReplyToMessage.From.FirstName + " " +
+                                        e.Message.ReplyToMessage.From.LastName,
+                                        e.Message.ReplyToMessage.From.Id));
+                                    break;
+                                case "/warn":
+                                    await Program.bot.SafeDeleteMessageAsync(e.Message);
+                                    Action.AddAction(new Warn(
+                                        e.Message.ReplyToMessage.From.Username,
+                                        e.Message.ReplyToMessage.From.FirstName + " " +
+                                        e.Message.ReplyToMessage.From.LastName,
+                                        e.Message.ReplyToMessage.From.Id));
+                                    break;
+                                case "/del":
+                                    await Program.bot.SafeDeleteMessageAsync(e.Message.ReplyToMessage);
+                                    await Program.bot.SafeDeleteMessageAsync(e.Message);
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            switch (cmd[0])
+                            {
+                                case "/add":
+                                    await Program.bot.SafeDeleteMessageAsync(e.Message);
+                                
+                                    if (cmd.Length < 2) return;
+
+                                    await Dictionary.Add(cmd[1]);
+                                    break;
+                                case "/delete":
+                                    await Program.bot.SafeDeleteMessageAsync(e.Message);
+                                    
+                                    if (cmd.Length < 2) return;
+
+                                    await Dictionary.Delete(cmd[1]);
+                                    break;
+                            }
+                        }
                     }
                     else
                     {
@@ -44,11 +109,9 @@ namespace ChatBot
                 {
                     switch (e.Message.Text)
                     {
-                        case "/cmd":
-                            // add word, del word, edit rules
-                            break;
                         case "/help":
-                            
+                            await HelpMessage.SendMessage();
+                            await Program.bot.SafeDeleteMessageAsync(e.Message);
                             break;
                         default:
                             await Program.bot.SafeDeleteMessageAsync(e.Message);
