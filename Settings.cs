2b0cb0917a8fc1bacd4999db268ba68a0c276c6b
Vadim Hanevych <vadhanevych@gmail.com>
2021-08-07 18:24:07 +0300

    Deploy bot. v1.0

--- Settings.cs
+++ Settings.cs
@@ -1,13 +1,17 @@
 using System.Collections.Generic;
 
+
 namespace ChatBot
 {
     public static class Settings
     {
         
-        public const long MainChat = 0;
-        public const long LogChat = 0;
+        public const long MainChat = -1001529627907;
         public const string InviteLinkIntoMainChat = "";
-        public static List<int> Admins = new List<int>();
+        
+        public const long LogChat = -577629424;
+        
+        public static List<int> Admins = new List<int>() {643559378, }; // 643559378
+
     }
 }
